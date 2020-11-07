7d3990c48b87d1afc58b1ae8c6c0e406ccbdd507
Harsh <thehjain@gmail.com>
2020-11-07 15:12:49 +0530

    Restore Sequence

--- LongChallenge/November_2020/RestoreSequence.java
+++ LongChallenge/November_2020/RestoreSequence.java
@@ -1,3 +1,8 @@
+/*
+.
+Restore Sequence
+.
+*/
 import java.util.*;
 import java.io.*;
 import java.math.*;
@@ -9,8 +14,12 @@ class RestoreSequence {
 	static int MOD = 1000000007;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+	static int maxN = 5000001;
+	static ArrayList<Integer> prime;
+
 	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
+		generatePrimes();
 		while (T-- != 0) {
 			solve();
 		}
@@ -18,7 +27,46 @@ class RestoreSequence {
 
 	static void solve() throws IOException {
 
-		System.out.println("hello");
+		int N = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().trim().split(" ");
+
+		int[] arr = new int[N];
+		int index = 0;
+
+		for (String ele : input)
+			arr[index++] = Integer.parseInt(ele);
+
+		solveUtil(arr, N);
+	}
+
+	static void solveUtil(int[] arr, int N) {
+		ArrayList<Integer> res = new ArrayList<>();
+		for (int i = 0; i < N; i++)
+			res.add(prime.get(arr[i]));
+
+		for (int ele : res)
+			System.out.print(ele + " ");
+
+		System.out.println();
+	}
+
+	static void generatePrimes() {
+
+		prime = new ArrayList<>();
+		boolean[] isPrime = new boolean[maxN];
+
+		Arrays.fill(isPrime, true);
+
+		for (int i = 2; i * i < maxN; i++) {
+			if (isPrime[i]) {
+				for (int j = i * i; j < maxN; j += i)
+					isPrime[j] = false;
+			}
+		}
+
+		for (int i = 2; i < maxN; i++)
+			if (isPrime[i])
+				prime.add(i);
 
 	}
 
