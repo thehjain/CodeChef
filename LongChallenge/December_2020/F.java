a926ce52b23a4213495b515515dd6d4c1f6e719f
Harsh <thehjain@gmail.com>
2020-12-07 13:10:07 +0530

    Empty Files

---
+++ LongChallenge/December_2020/F.java
@@ -0,0 +1,27 @@
+
+//Cute Chef Gift
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class F {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		while (T-- != 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+
+
+	}
+
+}
