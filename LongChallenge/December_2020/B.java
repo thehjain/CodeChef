1b3a36b727c32a6b67eacc8cae6d3f12a7c07204
Harsh <thehjain@gmail.com>
2020-12-07 13:56:04 +0530

    Even Pair Sum

--- LongChallenge/December_2020/B.java
+++ LongChallenge/December_2020/B.java
@@ -1,5 +1,5 @@
 
-//Cute Chef Gift
+//Even Pair Sum
 
 import java.util.*;
 import java.io.*;
@@ -9,9 +9,9 @@ class B {
 	private static int MAX = Integer.MAX_VALUE;
 	private static int MIN = Integer.MIN_VALUE;
 	private static int MOD = 1000000007;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		while (T-- != 0) {
 			solve();
@@ -20,7 +20,15 @@ class B {
 
 	static void solve() throws IOException {
 
+		String[] input = br.readLine().trim().split(" ");
 
+		long A = Long.parseLong(input[0]);
+		long B = Long.parseLong(input[1]);
+
+		if (A % 2 == 1 && B % 2 == 1)
+			System.out.println((A * B / 2) + 1);
+		else
+			System.out.println(A * B / 2);
 
 	}
 
