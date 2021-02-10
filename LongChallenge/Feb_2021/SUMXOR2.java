1b3afed0e6d02cfd09c2891a62e3c30c0bfa34c9
Harsh <thehjain@gmail.com>
2021-02-11 00:12:15 +0530

    XOR SUMS

--- LongChallenge/Feb_2021/SUMXOR2.java
+++ LongChallenge/Feb_2021/SUMXOR2.java
@@ -7,9 +7,11 @@ class SUMXOR2 {
 	private static int MAX = Integer.MAX_VALUE;
 	private static int MIN = Integer.MIN_VALUE;
 	private static int MOD = 998244353;
-	static FastScanner sc = new FastScanner();
+	static Reader sc = new Reader();
 	static StringBuilder sb = new StringBuilder();
+	// static long sum = 0;
 	static HashMap<Integer, Long> map = new HashMap<>();
+	static HashMap<Integer, Long> res = new HashMap<>();
 
 	public static void main(String[] args) throws IOException {
 		// int T = sc.nextInt();
@@ -24,94 +26,183 @@ class SUMXOR2 {
 
 		long[] arr = new long[n];
 
-		long allSum = 0;
-
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++)
 			arr[i] = sc.nextLong();
-			allSum += arr[i];
-		}
 
-		map.put(1, allSum);
+		int noOfQueries = sc.nextInt();
 
-		int queries = sc.nextInt();
+		while (noOfQueries-- > 0) {
 
-		while (queries-- > 0) {
-
-			int len = sc.nextInt();
-
+			int length = sc.nextInt();
 			long sum = 0;
 
-			if (len == 1) {
-				sb.append(map.get(1) + "\n");
+			if (res.containsKey(length)) {
+				sb.append(res.get(length) + "\n");
 				continue;
 			}
 
-			for (int i = 1; i <= len; i++) {
+			for (int i = 1; i <= length; i++) {
 				if (map.containsKey(i)) {
-					sum += map.get(i);
+					sum =  (sum % MOD + map.get(i) % MOD) % MOD;
 					continue;
 				}
-				findXor(arr, 0, n, i, 0);
-				sum += map.get(i);
-				// System.out.println(sum);
+				sum = (sum % MOD + findSum(arr, n, i, 0, 0) % MOD) % MOD;
 			}
+
+			res.put(length, sum);
 
 			sb.append(sum + "\n");
 
 		}
+
 		System.out.print(sb);
+	}
+
+	static long findSum(long[] arr, int n, int length, int currLength, long tempSum) {
+
+		// System.out.println(tempSum);2
+
+		if (currLength == length)
+			return tempSum % MOD;
+		if (n == 0)
+			return 0;
+		if (currLength > length)
+			return 0;
+
+		long temp = (findSum(arr, n - 1, length, currLength, tempSum) % MOD +
+		             findSum(arr, n - 1, length, currLength + 1, tempSum ^ arr[n - 1]) % MOD) % MOD;
+		map.put(length, temp);
+		return temp;
 
 	}
 
-	static void findXor(long[] arr, int pos, int n, int len, long curr) {
+	// static void findXor(long[] arr, int pos, int n, int len, long curr) {
 
-		if (pos == len) {
-			// System.out.println(curr);
-			if (!map.containsKey(len))
-				map.put(len, (long)0);
-			map.put(len, map.get(len) + curr % MOD);
-			return;
+	// 	if (pos == len) {
+	// 		// System.out.println(curr);
+	// 		if (!map.containsKey(len))
+	// 			map.put(len, (long)0);
+	// 		map.put(len, map.get(len) + curr % MOD);
+	// 		return;
+	// 	}
+
+	// 	for (int i = 0 ; i < n; i++) {
+	// 		// System.out.println(arr[i]);
+	// 		findXor(arr, pos + 1, n, len, curr ^ arr[i]);
+	// 	}
+
+	// }
+
+	static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
 		}
 
-		for (int i = 0 ; i < n; i++) {
-			// System.out.println(arr[i]);
-			findXor(arr, pos + 1, n, len, curr ^ arr[i]);
+		public Reader(String file_name) throws IOException {
+			din = new DataInputStream(
+			    new FileInputStream(file_name));
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
 		}
 
-	}
-
-	static class FastScanner {
-		public BufferedReader reader;
-		public StringTokenizer tokenizer;
-		public FastScanner() {
-			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
-			tokenizer = null;
+		public String readLine() throws IOException {
+			byte[] buf = new byte[64]; // line length
+			int cnt = 0, c;
+			while ((c = read()) != -1) {
+				if (c == '\n') {
+					if (cnt != 0) {
+						break;
+					} else {
+						continue;
+					}
+				}
+				buf[cnt++] = (byte)c;
+			}
+			return new String(buf, 0, cnt);
 		}
-		public String next() {
-			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-				try {
-					tokenizer = new StringTokenizer(reader.readLine());
-				} catch (IOException e) {
-					throw new RuntimeException(e);
+
+		public int nextInt() throws IOException {
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ') {
+				c = read();
+			}
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public long nextLong() throws IOException {
+			long ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public double nextDouble() throws IOException {
+			double ret = 0, div = 1;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (c == '.') {
+				while ((c = read()) >= '0' && c <= '9') {
+					ret += (c - '0') / (div *= 10);
 				}
 			}
-			return tokenizer.nextToken();
+
+			if (neg)
+				return -ret;
+			return ret;
 		}
-		public int nextInt() {
-			return Integer.parseInt(next());
+
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0,
+			                     BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
 		}
-		public long nextLong() {
-			return Long.parseLong(next());
+
+		private byte read() throws IOException {
+			if (bufferPointer == bytesRead)
+				fillBuffer();
+			return buffer[bufferPointer++];
 		}
-		public double nextDouble() {
-			return Double.parseDouble(next());
-		}
-		public String nextLine() {
-			try {
-				return reader.readLine();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+
+		public void close() throws IOException {
+			if (din == null)
+				return;
+			din.close();
 		}
 	}
 
