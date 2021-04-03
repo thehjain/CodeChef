01aef2b863752bb748cd2600ce74f475445e6686
Harsh <thehjain@gmail.com>
2021-04-03 13:45:49 +0530

    Chef And Dice

--- LongChallenge/March_2021/SDICE.java
+++ LongChallenge/March_2021/SDICE.java
@@ -8,17 +8,45 @@ class SDICE {
 	private static int MIN = Integer.MIN_VALUE;
 	private static int MOD = 1000000007;
 	static FastScanner sc = new FastScanner();
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		int T = sc.nextInt();
 		while (T-- > 0) {
 			solve();
 		}
+		System.out.print(sb);
 	}
 
 	static void solve() throws IOException {
 
-		System.out.println("hello");
+		long n = sc.nextLong();
+
+		if (n == 1) {
+			sb.append("20\n");
+			return;
+		}
+		if (n == 2) {
+			sb.append("36\n");
+			return;
+		}
+		if (n == 3) {
+			sb.append("51\n");
+			return;
+		}
+		if (n == 4) {
+			sb.append("60\n");
+			return;
+		}
+
+		long res = n * 11;
+		n %= 4;
+		if (n == 1) res += 21;
+		else if (n == 2) res += 22;
+		else if (n == 3) res += 22;
+		else res += 16;
+
+		sb.append(res + "\n");
 
 	}
 
